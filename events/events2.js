77f1e2b5db866624de39dbb209544f1b90b3daad
samirvithlani <samir.vithlani83955@gmail.com>
2022-10-18 15:19:01 +0530

    events

---
+++ events/events2.js
@@ -0,0 +1,23 @@
+function change(){
+
+    
+    document.getElementById("div1").innerHTML = "Hi";
+    document.getElementById("div1").style.backgroundColor = "red";
+    
+}
+function change1(){
+
+    document.getElementById("t1").innerHTML = "Hello....";
+
+}
+
+
+function change2(){
+
+    document.getElementById("t2").innerHTML = "Royal Technosoft";
+    
+}
+function change5(){
+
+        document.getElementById("div2").innerHTML="DEMO"
+}
---
+++ events/events3.js
@@ -0,0 +1,22 @@
+document.getElementById("btn").addEventListener("click",change);
+document.getElementById("print").addEventListener("mouseover",()=>{
+
+    alert("Hi");
+})
+
+function change(){
+
+    console.log("Hi");
+
+}
+
+function valid(){
+
+    console.log("Valid");
+
+}
+function load(){
+
+    console.log("Loaded");
+
+}
